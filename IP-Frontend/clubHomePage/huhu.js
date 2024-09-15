ff68d6146976bc78d33fe335278027664bc80311
madiha-ahmed-chowdhury <madihaahmedchowdhury6183@gmail.com>
2024-09-15 13:18:35 +0600

    Deleting extra scripts

--- IP-Frontend/clubHomePage/huhu.js
+++
@@ -1,9 +0,0 @@
-javascript: (function () {
-  var video = document.querySelector("video");
-  if (video) {
-    var speed = prompt("Enter playback speed:", "1.0");
-    video.playbackRate = parseFloat(speed);
-  } else {
-    alert("No video found!");
-  }
-})();
